b34fd3744b218da3456b74878fb7ac5d55efe131
Bealem123 <sintayehubealem411@gmail.com>
2025-01-27 01:09:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
       "http://localhost:3000", // Local frontend URL for development
-      "https://melodious-sunshine-207a7a.netlify.app/", // Deployed Netlify frontend URL (production)
-       "https://ubiquitous-empanada-8d0b8b.netlify.app/",
+      "https://melodious-sunshine-207a7a.netlify.app", // Deployed Netlify frontend URL (production)
+       "https://ubiquitous-empanada-8d0b8b.netlify.app",
     ];
 
     // Allow requests without an Origin header (e.g., Postman) or from allowed origins
