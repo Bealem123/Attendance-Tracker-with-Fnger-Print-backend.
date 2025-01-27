646ecb939cbcfd797bfca6a17c3bb5a41c6b48bd
Bealem123 <sintayehubealem411@gmail.com>
2025-01-27 14:52:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const corsOptions = {
       "http://localhost:3000", // Local frontend URL for development
       "https://melodious-sunshine-207a7a.netlify.app", // Deployed Netlify frontend URL (production)
        "https://ubiquitous-empanada-8d0b8b.netlify.app",
+       "https://darling-bombolone-f98b58.netlify.app",
+      
     ];
 
     // Allow requests without an Origin header (e.g., Postman) or from allowed origins
